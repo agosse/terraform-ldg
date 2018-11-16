f3abef461afa09ffbebb6d6dc8a271d1e82f3890
agosse <gosse.alex@gmail.com>
2018-11-16 00:26:42 -0800

    Deploying the application is not within scope.

--- vars.tf
+++ vars.tf
@@ -8,7 +8,6 @@ variable "ecr_name"                  {}
 variable "ecs_cluster_instance_type" {}
 variable "fe_bucket_id"              {}
 variable "fe_cert_arn"               {}
-variable "fe_content_dir"            {}
 variable "key_pair_name"             {}
 variable "region"                    {}
 variable "zone_name"                 {}
